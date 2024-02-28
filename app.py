41ad8c9c65de0788ed7eaffb37b69e233c9f3873
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-28 22:13:34 +0500

    upload file and show its content

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.sidebar.title('Whatsapp Chat Analyzer')
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode('utf-8')
+    st.text(data)
