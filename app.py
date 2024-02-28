951915e0e7b843b03c3f1a155c952f9c52fe59f3
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-28 22:16:40 +0500

    showed processed data (dataframe) on screen

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from preprocessor import preprocess
 
 st.sidebar.title('Whatsapp Chat Analyzer')
 
@@ -6,4 +7,6 @@ uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode('utf-8')
-    st.text(data)
+    df = preprocess(data)
+
+    st.dataframe(df)
