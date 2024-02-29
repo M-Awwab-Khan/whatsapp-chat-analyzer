fa47fe539b797fd8f6d08cf62495edfa61a966d4
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-29 18:11:14 +0500

    most active users bar chart

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from preprocessor import preprocess
-from helper import fetch_stats
+from helper import fetch_stats, most_active_users
+import matplotlib.pyplot as plt
 
 st.sidebar.title('Whatsapp Chat Analyzer')
 
@@ -22,7 +23,7 @@ if uploaded_file is not None:
     if st.sidebar.button('Show Analysis'):
         col1, col2, col3, col4 = st.columns(4)
 
-        num_messages, num_words, num_media = fetch_stats(selected_user, df)
+        num_messages, num_words, num_media, num_links = fetch_stats(selected_user, df)
         with col1:
             st.header('Total Messages')
             st.title(num_messages)
@@ -34,3 +35,20 @@ if uploaded_file is not None:
         with col3:
             st.header("Media Shared")
             st.title(num_media)
+
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
+
+        # finding the active users in the group(Group level)
+        if selected_user == 'Overall':
+            st.title('Most Active Users')
+            x = most_active_users(df)
+            fig, ax = plt.subplots()
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index, x.values, color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
