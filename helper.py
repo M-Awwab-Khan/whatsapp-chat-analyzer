c67352708b44d2beff55a64e7846f3666e690637
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-29 17:39:55 +0500

    now returns both num words and num messages

--- helper.py
+++ helper.py
@@ -1,7 +1,16 @@
 def fetch_stats(selected_user, df):
 
     if selected_user == 'Overall':
-        return df.shape[0]
+        num_messages = df.shape[0]
+        num_words = 0
+        for msg in df['message'].tolist():
+            num_words += len(msg.split())
+        return num_messages, num_words
     
     else:
-        return df[df['user'] == selected_user].shape[0]
+        user_df = df[df['user'] == selected_user]
+        num_messages = user_df.shape[0]
+        num_words = 0
+        for msg in user_df['message'].tolist():
+            num_words += len(msg.split())
+        return num_messages, num_words
