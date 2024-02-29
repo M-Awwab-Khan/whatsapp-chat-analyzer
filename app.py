62d571d63c22e1bab36940d70d7d3fbe8a8f0164
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-29 17:20:52 +0500

    total messages of user in 1st column

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from preprocessor import preprocess
+from helper import fetch_stats
 
 st.sidebar.title('Whatsapp Chat Analyzer')
 
@@ -16,7 +17,11 @@ if uploaded_file is not None:
     user_list.sort()
     user_list.insert(0, 'Overall')
 
-    st.sidebar.selectbox('Analysis with respect to', user_list)
+    selected_user = st.sidebar.selectbox('Analysis with respect to', user_list)
 
     if st.sidebar.button('Show Analysis'):
-        ...
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header('Total Messages')
+            st.title(fetch_stats(selected_user, df))
