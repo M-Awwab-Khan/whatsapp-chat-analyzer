6d4ddddc72194901639363bf6440b74a550790b1
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-29 17:41:27 +0500

    concise code

--- helper.py
+++ helper.py
@@ -1,16 +1,10 @@
 def fetch_stats(selected_user, df):
 
-    if selected_user == 'Overall':
-        num_messages = df.shape[0]
-        num_words = 0
-        for msg in df['message'].tolist():
-            num_words += len(msg.split())
-        return num_messages, num_words
-    
-    else:
-        user_df = df[df['user'] == selected_user]
-        num_messages = user_df.shape[0]
-        num_words = 0
-        for msg in user_df['message'].tolist():
-            num_words += len(msg.split())
-        return num_messages, num_words
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+        
+    num_messages = df.shape[0]
+    num_words = 0
+    for msg in df['message'].tolist():
+        num_words += len(msg.split())
+    return num_messages, num_words
