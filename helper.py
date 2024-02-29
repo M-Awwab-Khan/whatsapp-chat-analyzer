5b8f28a0875f235ae16b3c55e0897dc9c40c6b10
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-02-29 18:11:24 +0500

    most active users logic

--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+from urlextract import URLExtract
+extractor = URLExtract()
+
 def fetch_stats(selected_user, df):
 
     if selected_user != 'Overall':
@@ -6,6 +9,11 @@ def fetch_stats(selected_user, df):
     num_messages = df.shape[0]
     num_words = 0
     num_media = df[df['message'] == '<Media omitted>\n'].shape[0]
-    for msg in df['message'].tolist():
+    num_links = 0
+    for msg in df['message']:
+        num_links += len(extractor.find_urls(msg))
         num_words += len(msg.split())
-    return num_messages, num_words, num_media
+    return num_messages, num_words, num_media, num_links
+
+def most_active_users(df):
+    return df['user'].value_counts().head()
