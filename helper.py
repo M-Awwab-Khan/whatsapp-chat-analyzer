02218c84d2c64a4d6f9a1f73f6a80ed7b86be9c6
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-01 17:52:43 +0500

    word cloud df function

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 from urlextract import URLExtract
+from wordcloud import WordCloud
 extractor = URLExtract()
 
 def fetch_stats(selected_user, df):
@@ -19,3 +20,12 @@ def most_active_users(df):
     active_users = df['user'].value_counts().head()
     proportion_active_users = round(df['user'].value_counts(normalize=True)*100, 2).reset_index().rename(columns={'index': 'name', 'user': 'percent'})
     return active_users, proportion_active_users
+
+def create_wordcloud(selected_user, df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
