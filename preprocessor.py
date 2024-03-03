d70b77e4445049036c8c0d2841415b90c1f44be4
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-03 22:16:31 +0500

    month_num column

--- preprocessor.py
+++ preprocessor.py
@@ -29,5 +29,6 @@ def preprocess(data):
     df['day'] = df['date'].dt.day
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
+    df['month_num'] = df['date'].dt.month
 
     return df
